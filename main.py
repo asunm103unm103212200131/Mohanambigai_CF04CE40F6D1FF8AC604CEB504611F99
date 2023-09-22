4c8c533a585b5519b1e53a5b6623eaef16c52d67
asunm103unm103212200131 <144413061+asunm103unm103212200131@users.noreply.github.com>
2023-09-22 17:05:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-class BankAccount:
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name 
-    self.__account_balance = initial_balance 
-  def deposit(self, amount):
-    if amount > 0:
-      self. __account_balance += amount 
-      print("Deposited ₹{}.New Balance: ₹{}".format(amount, self. __account_balance)) 
-    else:
-      print("Invalid deposit amount. Please deposit a positive amount.")
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self. __account_balance:
-      self.__account_balance -= amount
-      print("Withdrew ₹{}. New Balance: ₹{}".format(amount, self. __account_balance))
-    else: 
-      print("Invalid withdrawal amount or insufficient balance.")
-  def display_balance(self):
-      print("Account balance for {} (Account #{}): ₹{}".format(self.__account_holder_name,self.__account_number, self. __account_balance))
-account = BankAccount(account_number="123456789", account_holder_name= "Vennila",initial_balance=5000.0)
-account.display_balance() 
-account.deposit(500.0)
-account.withdraw(200.0)
-account.display_balance()
+class Student:
+   def __init__(self, name, roll_number, cgpa) :
+     self.name = name
+     self.roll_number = roll_number
+     self.cgpa = cgpa
+def sort_students(student_list):
+  sorted_students = sorted(student_list, key = lambda student:student.cgpa, reverse = True) 
+  return sorted_students
+students = [Student("Hari", "A123", 7.8), Student(" Srikanth", "A124", 8.9) , Student("Saumya", "A125",9.1), Student ("Mahidhar", "A126",9.9) ]
+sorted_students = sort_students(students) 
+for student in sorted_students:
+    print(" Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
